31024592d6efffec54c0587d916214856e88f8e7
HPASUS8 <76400381+HPASUS8@users.noreply.github.com>
2023-06-18 13:40:38 +0300

    Обновление 2.3

--- test.py
+++ test.py
@@ -1,10 +1,12 @@
 name = input ('Привет! Как тебя зовут? ')
 age = int(input('А сколько тебе лет? '))
 year = int(int(input('Напомнишь, какой сейчас год? ')))
+
 hyear = (100 - age) + year
 hyear1 = (100 - age) + (year - 1)
 hby = year - age
 hby1 = year - age - 1
+
 print (f'Привет, {name}! Сейчас тебе {age}, ты родился(ась) в {hby}! Или в {hby1}? В любом случае, в {hyear}/{hyear1} году тебе исполнится 100 лет!')
 print (f'А сейчас я скажу твоё имя наоборот:')
 print (name[::-1])
