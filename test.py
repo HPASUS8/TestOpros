e09dff1704524a367df160cd277d7575dc182296
HPASUS8 <76400381+HPASUS8@users.noreply.github.com>
2023-06-18 13:43:59 +0300

    Обновление 3.0

--- test.py
+++ test.py
@@ -2,12 +2,12 @@ name = input ('Привет! Как тебя зовут? ')
 age = int(input('А сколько тебе лет? '))
 year = int(int(input('Напомнишь, какой сейчас год? ')))
 
-hyear = (100 - age) + year
-hyear1 = (100 - age) + (year - 1)
-hby = year - age
-hby1 = year - age - 1
+one_hundred_year = (100 - age) + year
+one_hundred_year_1 = (100 - age) + (year - 1)
+happy_bday = year - age
+happy_bday_1 = year - age - 1
 
-print (f'Привет, {name}! Сейчас тебе {age}, ты родился(ась) в {hby}! Или в {hby1}? В любом случае, в {hyear}/{hyear1} году тебе исполнится 100 лет!')
-print (f'А сейчас я скажу твоё имя наоборот:')
-print (name[::-1])
-print (f'Ну что, я удивил тебя? Надеюсь, да!')
+print(f'Привет, {name}! Сейчас тебе {age}, ты родился(ась) в {hby}! Или в {hby1}? В любом случае, в {hyear}/{hyear1} году тебе исполнится 100 лет!')
+print(f'А сейчас я скажу твоё имя наоборот:')
+print(name[::-1])
+print(f'Ну что, я удивил тебя? Надеюсь, да!')
